5d0c5131c5fcb9b61aefd7598cad6b6f10cdfd39
Besin <0321sjj@naver.com>
2024-06-19 20:33:20 +0900

    Feat : GameManager, GameData, DataManager
    게임 저장 및 불러오기 기능 추가 // 테스트 필요함

---
+++ Assets/GameManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private GameData gameData;
+    private float saveInterval = 0.1f;
+    private Coroutine autoSaveContinue;
+
+    public int currentScore;
+    public int currentMoney;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        return;
+    }
+
+    private void Start()
+    {
+        gameData = DataManager.instance.GameLoad();
+        if (gameData == null)
+        {
+            gameData = new GameData();
+        }
+    }
+
+    private void Update()
+    {
+        StartAutoSave();
+    }
+
+    void OnApplicationQuit()
+    {
+        DataManager.instance.GameSave(gameData);
+    }
+
+    public void StartAutoSave()
+    {
+        if (autoSaveContinue == null)
+        {
+            autoSaveContinue = StartCoroutine(AutoSave());
+        }
+    }
+
+    IEnumerator AutoSave()
+    {
+        while(true)
+        {
+            gameData.GameScore = currentScore;
+            gameData.GameMoney = currentMoney;
+            yield return new WaitForSeconds(saveInterval);
+        }
+    }
+}
